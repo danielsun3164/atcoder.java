0dc845221a23b483629fff3346496f7834cc67b7
Daniel Sun <danielsun3164@gmail.com>
2020-08-20 15:58:09 +0900

    abc099を追加

---
+++ src/test/java/abc099/ProblemCTest.java
@@ -0,0 +1,23 @@
+package abc099;
+
+import org.junit.jupiter.api.Test;
+
+import practice.TestBase;
+
+class ProblemCTest extends TestBase {
+
+	@Test
+	void case1() {
+		check("127", "4");
+	}
+
+	@Test
+	void case2() {
+		check("3", "3");
+	}
+
+	@Test
+	void case3() {
+		check("44852", "16");
+	}
+}
