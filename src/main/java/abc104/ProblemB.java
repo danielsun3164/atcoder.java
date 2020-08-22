8fb9fb26c1bfb231811024c5d1e69f78b8626c40
Daniel Sun <danielsun3164@gmail.com>
2020-08-22 18:06:37 +0900

    abc104を追加

---
+++ src/main/java/abc104/ProblemB.java
@@ -0,0 +1,25 @@
+package abc104;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class ProblemB {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			char[] s = scanner.next().toCharArray();
+			boolean result = true;
+			if ('A' != s[0]) {
+				// 最初の文字が'A'でない
+				result = false;
+			} else if (1 != IntStream.rangeClosed(2, s.length - 2).filter(i -> 'C' == s[i]).count()) {
+				// 3文字目から最後から2文字まで'C'の数が1でない
+				result = false;
+			} else if (2 != IntStream.range(0, s.length).filter(i -> ('A' <= s[i]) && (s[i] <= 'Z')).count()) {
+				// 最初の'A'と'C'以外はすべて小文字でない
+				result = false;
+			}
+			System.out.println(result ? "AC" : "WA");
+		}
+	}
+}
---
+++ src/main/java/abc104/ProblemD.java
@@ -0,0 +1,33 @@
+package abc104;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * 解説どおりに作成したソースコード
+ */
+public class ProblemD {
+
+	/** 文字列ABC */
+	private static final char[] ABC = new char[] { 'A', 'B', 'C' };
+	/** 文字? */
+	private static final char ALL = '?';
+	/** 1_000_000_007 */
+	private static final long MOD = 1_000_000_007L;
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			char[] s = scanner.next().toCharArray();
+			int n = s.length;
+			long[][] dp = new long[n + 1][4];
+			dp[n][3] = 1L;
+			dp[n][0] = dp[n][1] = dp[n][2] = 0L;
+			IntStream.rangeClosed(1, n).map(i -> n - i).forEach(i -> {
+				dp[i][3] = ((ALL == s[i]) ? 3 : 1) * dp[i + 1][3] % MOD;
+				IntStream.range(0, 3).forEach(j -> dp[i][j] = (((ALL == s[i]) ? 3 : 1) * dp[i + 1][j] % MOD
+						+ (((ALL == s[i]) || (ABC[j] == s[i])) ? 1 : 0) * dp[i + 1][j + 1] % MOD) % MOD);
+			});
+			System.out.println(dp[0][0]);
+		}
+	}
+}
