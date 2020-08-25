c30a31eb2dcf17e87b90918330503940f3f509dc
Daniel Sun <danielsun3164@gmail.com>
2020-08-25 20:01:25 +0900

    abc176を追加

---
+++ src/main/java/abc176/ProblemA.java
@@ -0,0 +1,13 @@
+package abc176;
+
+import java.util.Scanner;
+
+public class ProblemA {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int n = scanner.nextInt(), x = scanner.nextInt(), t = scanner.nextInt();
+			System.out.println((n + x - 1) / x * t);
+		}
+	}
+}
