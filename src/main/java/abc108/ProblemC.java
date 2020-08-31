30198c078347ad81e4029ae625ba40a46d36c6a7
Daniel Sun <danielsun3164@gmail.com>
2020-08-31 18:50:16 +0900

    abc108を追加

---
+++ src/main/java/abc108/ProblemC.java
@@ -0,0 +1,20 @@
+package abc108;
+
+import java.util.Scanner;
+
+public class ProblemC {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int n = scanner.nextInt(), k = scanner.nextInt();
+			long count = n / k;
+			long answer = count * count * count;
+			if (0 == (k & 1)) {
+				// kが2の倍数のとき
+				long count2 = n / k + ((n % k >= k / 2) ? 1 : 0);
+				answer += count2 * count2 * count2;
+			}
+			System.out.println(answer);
+		}
+	}
+}
