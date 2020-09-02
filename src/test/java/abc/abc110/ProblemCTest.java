99989889eb77ff5126e74e36790a9f256efd9367
Daniel Sun <danielsun3164@gmail.com>
2020-09-02 19:18:27 +0900

    abc110を追加

---
+++ src/test/java/abc/abc110/ProblemCTest.java
@@ -0,0 +1,23 @@
+package abc.abc110;
+
+import org.junit.jupiter.api.Test;
+
+import practice.TestBase;
+
+class ProblemCTest extends TestBase {
+
+	@Test
+	void case1() {
+		check("azzel\n" + "apple", "Yes");
+	}
+
+	@Test
+	void case2() {
+		check("chokudai\n" + "redcoder", "No");
+	}
+
+	@Test
+	void case3() {
+		check("abcdefghijklmnopqrstuvwxyz\n" + "ibyhqfrekavclxjstdwgpzmonu", "Yes");
+	}
+}
