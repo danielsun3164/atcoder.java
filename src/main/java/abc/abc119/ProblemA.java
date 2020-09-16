6c9523b2e5be2e53510f8bdc2d9255421317b77c
Daniel Sun <danielsun3164@gmail.com>
2020-09-16 18:14:32 +0900

    abc119を追加

---
+++ src/main/java/abc/abc119/ProblemA.java
@@ -0,0 +1,17 @@
+package abc.abc119;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ProblemA {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			String s = scanner.next();
+			LocalDate date = LocalDate.parse(s, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+			LocalDate date0430 = LocalDate.of(2019, 4, 30);
+			System.out.println((date.isAfter(date0430)) ? "TBD" : "Heisei");
+		}
+	}
+}
---
+++ src/test/java/abc/abc119/ProblemATest.java
@@ -0,0 +1,18 @@
+package abc.abc119;
+
+import org.junit.jupiter.api.Test;
+
+import practice.TestBase;
+
+class ProblemATest extends TestBase {
+
+	@Test
+	void case1() {
+		check("2019/04/30", "Heisei");
+	}
+
+	@Test
+	void case2() {
+		check("2019/11/01", "TBD");
+	}
+}
