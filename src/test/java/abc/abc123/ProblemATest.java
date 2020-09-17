708092c784f8f9308013083e91fe858b2c80e455
Daniel Sun <danielsun3164@gmail.com>
2020-09-17 14:44:03 +0900

    abc123を追加

---
+++ src/test/java/abc/abc123/ProblemATest.java
@@ -0,0 +1,18 @@
+package abc.abc123;
+
+import org.junit.jupiter.api.Test;
+
+import practice.TestBase;
+
+class ProblemATest extends TestBase {
+
+	@Test
+	void case1() {
+		check("1\n" + "2\n" + "4\n" + "8\n" + "9\n" + "15", "Yay!");
+	}
+
+	@Test
+	void case2() {
+		check("15\n" + "18\n" + "26\n" + "35\n" + "36\n" + "18", ":(");
+	}
+}
