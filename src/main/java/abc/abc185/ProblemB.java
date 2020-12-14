ba637e08296eb8844bafb7813802769551c206f3
Daniel Sun <danielsun3164@gmail.com>
2020-12-14 18:45:17 +0900

    b[m]からtまでの時間の判定方法を変更

--- src/main/java/abc/abc185/ProblemB.java
+++ src/main/java/abc/abc185/ProblemB.java
@@ -8,14 +8,15 @@ public class ProblemB {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int n = scanner.nextInt(), m = scanner.nextInt(), t = scanner.nextInt();
-			int[] a = new int[m + 1], b = new int[m + 1];
+			int[] a = new int[m + 2], b = new int[m + 2];
 			a[0] = b[0] = 0;
 			IntStream.rangeClosed(1, m).forEach(i -> {
 				a[i] = scanner.nextInt();
 				b[i] = scanner.nextInt();
 			});
+			a[m + 1] = b[m + 1] = t;
 			long x = n;
-			for (int i = 1; i <= m; i++) {
+			for (int i = 1; i <= m + 1; i++) {
 				// 電池が減る
 				x -= a[i] - b[i - 1];
 				if (x <= 0) {
@@ -28,12 +29,7 @@ public class ProblemB {
 					x = n;
 				}
 			}
-			x -= t - b[m];
-			if (x <= 0) {
-				System.out.println("No");
-			} else {
-				System.out.println("Yes");
-			}
+			System.out.println("Yes");
 		}
 	}
 }
