823f2961beda7f86fe9e8c6716fd11c679d34f18
Daniel Sun <danielsun3164@gmail.com>
2021-02-13 19:54:48 +0900

    不要なnew int[][]を削除

--- src/main/java/other/joisc2007/ProblemL_Packing1.java
+++ src/main/java/other/joisc2007/ProblemL_Packing1.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class ProblemL_Packing1 {
 
 	public static void main(String[] args) {
-		int[][] result = new int[][] { { 18768060, 18768060 }, { 81231940, 18768060 }, { 50000000, 39589353 },
+		int[][] result = { { 18768060, 18768060 }, { 81231940, 18768060 }, { 50000000, 39589353 },
 				{ 18768060, 60410647 }, { 81231940, 60410647 }, { 50000000, 81231940 } };
 		for (int[] a : result) {
 			System.out.println(Arrays.stream(a).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
