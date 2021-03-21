4a773c0694ed3d2c34695912fb456ff500bca6b1
Daniel Sun <danielsun3164@gmail.com>
2021-03-21 23:44:20 +0900

    数字のリテラル表記を読みやすく修正

--- src/main/java/abc/abc151_200/abc196/ProblemC.java
+++ src/main/java/abc/abc151_200/abc196/ProblemC.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class ProblemC {
 
 	/** 倍数となる基数一覧 */
-	private static final long[] KEYS = { 11, 101, 1001, 10001, 100001, 1000001 };
+	private static final long[] KEYS = { 11L, 101L, 1_001L, 1_0001L, 1_00001L, 1_000001L };
 	/** 条件を見なす数字の最小値の一覧 */
-	private static final long[] MINS = { 11, 1010, 100100, 10001000, 1000010000L, 100000100000L };
+	private static final long[] MINS = { 11L, 10_10L, 100100, 1000_1000L, 10000_10000L, 100000_100000L };
 	/** 条件を見なす数字の最大値の一覧 */
-	private static final long[] MAXS = { 99, 9999, 999999, 99999999, 9999999999L, 999999999999L };
+	private static final long[] MAXS = { 99L, 99_99L, 999_999L, 9999_9999L, 99999_99999L, 999999_999999L };
 	/** それぞれ条件を見なす数字の数 */
 	private static final int[] RESULTS = { 9, 90, 900, 9000, 90000, 900000 };
 
