330c672cdb9a654d9c37082cc3e11aced0017413
Daniel Sun <danielsun3164@gmail.com>
2021-03-29 02:57:37 +0900

    各色の最小値、最大値の求め方を変更

--- src/main/java/abc/abc151_200/abc197/ProblemE.java
+++ src/main/java/abc/abc151_200/abc197/ProblemE.java
@@ -1,10 +1,7 @@
 package abc.abc151_200.abc197;
 
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.IntStream;
 
 /**
@@ -12,48 +9,39 @@ import java.util.stream.IntStream;
  */
 public class ProblemE {
 
+	/** 最大値 */
+	private static final int INF = Integer.MAX_VALUE >> 1;
+
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int n = scanner.nextInt();
-			SortedMap<Integer, SortedSet<Integer>> map = new TreeMap<>();
+			// min[i]は色iの最小値、max[i]は色iの最大値
+			int[] min = new int[n + 1], max = new int[n + 1];
+			Arrays.fill(min, INF);
+			Arrays.fill(max, -INF);
+			min[0] = max[0] = 0;
 			IntStream.range(0, n).forEach(i -> {
 				int x = scanner.nextInt(), c = scanner.nextInt();
-				SortedSet<Integer> set = map.get(c);
-				if (null == set) {
-					set = new TreeSet<>();
-					map.put(c, set);
-				}
-				set.add(x);
-			});
-			// coord[i][0]は色iの最小座標、coord[i][1]は色iの最大座標
-			int[][] coord = new int[n + 1][2];
-			coord[0][0] = coord[0][1] = 0;
-			IntStream.rangeClosed(1, n).forEach(i -> {
-				if (map.containsKey(i)) {
-					coord[i][0] = map.get(i).first();
-					coord[i][1] = map.get(i).last();
-				} else {
-					coord[i][0] = coord[i - 1][0];
-					coord[i][1] = coord[i - 1][1];
-				}
+				min[c] = Math.min(min[c], x);
+				max[c] = Math.max(max[c], x);
 			});
 			// dp[i][0]は色iの最小座標まで収集した結果、dp[i][1]は色iの最大座標まで収集した結果
 			long[][] dp = new long[n + 1][2];
 			dp[0][0] = dp[0][1] = 0L;
 			IntStream.rangeClosed(1, n).forEach(i -> {
-				if (map.containsKey(i)) {
-					dp[i][0] = Math.min(
-							dp[i - 1][0] + Math.abs(coord[i][1] - coord[i - 1][0]) + (coord[i][1] - coord[i][0]),
-							dp[i - 1][1] + Math.abs(coord[i][1] - coord[i - 1][1]) + (coord[i][1] - coord[i][0]));
-					dp[i][1] = Math.min(
-							dp[i - 1][0] + Math.abs(coord[i][0] - coord[i - 1][0]) + (coord[i][1] - coord[i][0]),
-							dp[i - 1][1] + Math.abs(coord[i][0] - coord[i - 1][1]) + (coord[i][1] - coord[i][0]));
-				} else {
+				if (INF == min[i]) {
 					dp[i][0] = dp[i - 1][0];
 					dp[i][1] = dp[i - 1][1];
+					min[i] = min[i - 1];
+					max[i] = max[i - 1];
+				} else {
+					dp[i][0] = Math.min(dp[i - 1][0] + Math.abs(max[i] - min[i - 1]) + (max[i] - min[i]),
+							dp[i - 1][1] + Math.abs(max[i] - max[i - 1]) + (max[i] - min[i]));
+					dp[i][1] = Math.min(dp[i - 1][0] + Math.abs(min[i] - min[i - 1]) + (max[i] - min[i]),
+							dp[i - 1][1] + Math.abs(min[i] - max[i - 1]) + (max[i] - min[i]));
 				}
 			});
-			System.out.println(Math.min(dp[n][0] + Math.abs(coord[n][0]), dp[n][1] + Math.abs(coord[n][1])));
+			System.out.println(Math.min(dp[n][0] + Math.abs(min[n]), dp[n][1] + Math.abs(max[n])));
 		}
 	}
 }
