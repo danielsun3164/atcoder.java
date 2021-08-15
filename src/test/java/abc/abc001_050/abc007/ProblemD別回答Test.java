9921af7e4d41da2a52bedcbdd3e8b5e858703c42
Daniel Sun <danielsun3164@gmail.com>
2021-08-16 02:35:40 +0900

    動的計画法の実装方法を追加

---
+++ src/test/java/abc/abc001_050/abc007/ProblemD別回答Test.java
@@ -0,0 +1,28 @@
+package abc.abc001_050.abc007;
+
+import org.junit.jupiter.api.Test;
+
+import testbase.TestBase;
+
+class ProblemD別回答Test extends TestBase {
+
+	@Test
+	void case1() {
+		check("1 9", "2");
+	}
+
+	@Test
+	void case2() {
+		check("40 49", "10");
+	}
+
+	@Test
+	void case3() {
+		check("1 1000", "488");
+	}
+
+	@Test
+	void case4() {
+		check("1 1000000000000000000", "981985601490518016");
+	}
+}
