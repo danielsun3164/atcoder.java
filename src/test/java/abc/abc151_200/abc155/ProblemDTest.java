7af33d6a0c9694c3665645581deb5a83da34743b
Daniel Sun <danielsun3164@gmail.com>
2022-01-11 17:07:17 +0900

    不要なテストケースを削除

--- src/test/java/abc/abc151_200/abc155/ProblemDTest.java
+++ src/test/java/abc/abc151_200/abc155/ProblemDTest.java
@@ -1,7 +1,5 @@
 package abc.abc151_200.abc155;
 
-import java.io.File;
-
 import org.junit.jupiter.api.Test;
 
 import testbase.TestBase;
@@ -23,10 +21,5 @@ class ProblemDTest extends TestBase {
 		check("30 413\n"
 				+ "-170202098 -268409015 537203564 983211703 21608710 -443999067 -937727165 -97596546 -372334013 398994917 -972141167 798607104 -949068442 -959948616 37909651 0 886627544 -20098238 0 -948955241 0 -214720580 277222296 -18897162 834475626 0 -425610555 110117526 663621752 0",
 				"448283280358331064");
-	}
-
-	@Test
-	void case4() {
-		check(new File("/daniel/tmp/20220102/in/sub1_02.txt"), new File("/daniel/tmp/20220102/out/sub1_02.txt"));
 	}
 }
