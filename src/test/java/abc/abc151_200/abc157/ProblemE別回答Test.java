4b4d721915cd564b67e52d6f325681668790b229
Daniel Sun <danielsun3164@gmail.com>
2022-03-15 01:56:45 +0900

    テストケースの想定結果の改行をLFに統一する

--- src/test/java/abc/abc151_200/abc157/ProblemE別回答Test.java
+++ src/test/java/abc/abc151_200/abc157/ProblemE別回答Test.java
@@ -9,6 +9,6 @@ class ProblemE別回答Test extends TestBase {
 	@Test
 	void case1() {
 		check("7\n" + "abcdbbd\n" + "6\n" + "2 3 6\n" + "1 5 z\n" + "2 1 1\n" + "1 4 a\n" + "1 7 d\n" + "2 1 7",
-				"3\n" + "1\n" + "5");
+				"3" + LF + "1" + LF + "5");
 	}
 }
