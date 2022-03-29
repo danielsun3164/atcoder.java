863c18a595d02831f4d38aec7b3dccf60ae0eae3
Daniel Sun <danielsun3164@gmail.com>
2022-03-29 14:31:05 +0900

    ローカル用のテストケースを削除

--- src/test/java/abc/abc201_250/abc213/ProblemHTest.java
+++ src/test/java/abc/abc201_250/abc213/ProblemHTest.java
@@ -1,7 +1,5 @@
 package abc.abc201_250.abc213;
 
-import java.io.File;
-
 import org.junit.jupiter.api.Test;
 
 import testbase.TestBase;
@@ -21,41 +19,5 @@ class ProblemHTest extends TestBase {
 	@Test
 	void case3() {
 		check("2 1 5\n" + "1 2\n" + "31415 92653 58979 32384 62643", "844557977");
-	}
-
-	@Test
-	void case4() {
-		check(new File("/daniel/tmp/20220328/in/03_t_max_perfect_00.txt"),
-				new File("/daniel/tmp/20220328/out/03_t_max_perfect_00.txt"));
-	}
-
-	@Test
-	void case5() {
-		check(new File("/daniel/tmp/20220328/in/04_t_max_cycle_00.txt"),
-				new File("/daniel/tmp/20220328/out/04_t_max_cycle_00.txt"));
-	}
-
-	@Test
-	void case6() {
-		check(new File("/daniel/tmp/20220328/in/05_t_max_anti_matrix_00.txt"),
-				new File("/daniel/tmp/20220328/out/05_t_max_anti_matrix_00.txt"));
-	}
-
-	@Test
-	void case7() {
-		check(new File("/daniel/tmp/20220328/in/06_t_max_random_00.txt"),
-				new File("/daniel/tmp/20220328/out/06_t_max_random_00.txt"));
-	}
-
-	@Test
-	void case8() {
-		check(new File("/daniel/tmp/20220328/in/06_t_max_random_01.txt"),
-				new File("/daniel/tmp/20220328/out/06_t_max_random_01.txt"));
-	}
-
-	@Test
-	void case9() {
-		check(new File("/daniel/tmp/20220328/in/06_t_max_random_02.txt"),
-				new File("/daniel/tmp/20220328/out/06_t_max_random_02.txt"));
 	}
 }
