ce7eb60c03663a1b7134682f353dfb90979a4826
Daniel Sun <danielsun3164@gmail.com>
2024-12-25 03:01:50 +0900

    importするクラスを変更

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -1,7 +1,7 @@
 package testbase;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
